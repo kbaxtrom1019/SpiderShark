373f24d814c157de0a601a5ac6cd35c407421920
Kevin Baxtrom <kbaxtrom1019>
2016-09-14 20:57:34 -0500

    Rough API
    - Got APIKey authentication in using example code
    - Also sumitting test harness for HMACAuth

---
+++ SpiderSharkAPI/Controllers/LeaderBoardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SpiderSharkAPI.Models;
+using SpiderSharkAPI.Filters;
+
+namespace SpiderSharkAPI.Controllers
+{
+    public class LeaderboardController : ApiController
+    {
+
+        [HttpPost]
+        [HMACAuth]
+        [ActionName("Upload")]
+        public IHttpActionResult PostLeaderboardEntry(LeaderboardEntry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(entry);
+            }
+            else
+            {
+                return BadRequest("Invalid leader board entry:" + entry.ToString());
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult Status(int pageSize, int pageIndex)
+        {
+            List<LeaderboardEntry> data = new List<LeaderboardEntry>();
+            data.Add(new LeaderboardEntry());
+            data.Add(new LeaderboardEntry());
+            return Ok(data);
+        }
+    }
+}
---
+++ SpiderSharkAPI/Models/LeaderBoardEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpiderSharkAPI.Models
+{
+    public class LeaderboardEntry
+    {
+        [Required]
+        [MaxLength(140)]
+        public string UserName { get; set; }
+        [Required]
+        public int Score { get; set; }
+    }
+}
